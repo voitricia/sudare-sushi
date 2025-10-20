b0cc0e775b5007fa005811c9fbd70a7b908112e6
voitricia <a.dona.voigt@gmail.com>
2025-10-20 20:28:55 -0300

    arquivos spring boot

---
+++ ifsc/src/main/java/com/sudare/ifsc/SudareApplication.java
@@ -0,0 +1,13 @@
+package com.sudare.ifsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudareApplication.class, args);
+	}
+
+}
---
+++ ifsc/src/test/java/com/sudare/ifsc/SudareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudare.ifsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
